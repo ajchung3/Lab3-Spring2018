5bdfc04740990dc684de97838cbe644c858cc5d4
chung <ajchung3@illinois.edu>
2018-09-19 20:45:40 -0500

    Initial project commit

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,34 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 public class WebScraper {
     public static void main(String[] unused) {
-        System.out.println("Ran");
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+    }
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        String[] wordCount;
+        int princeCount = 0;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        wordCount = contents.split(" ");
+        for (int i = 0; i < wordCount.length; i++) {
+            if (wordCount[i].equalsIgnoreCase("prince") || wordCount[i].equalsIgnoreCase("prince,")||wordCount[i].equalsIgnoreCase("prince.")) {
+                princeCount++;
+            }
+        }
+        urlScanner.close();
+        return contents + "Word Count: " + wordCount.length + "     Prince Count: " + princeCount;
     }
 }
